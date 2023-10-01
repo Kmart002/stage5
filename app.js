12893905a3e4b226e9d6e2d05824d94b5c6ebe90
Kmart002 <128054511+Kmart002@users.noreply.github.com>
2023-10-01 20:37:35 +0100

    cors

--- app.js
+++ app.js
@@ -1,42 +1,53 @@
 
-const express = require ('express');
+const express = require('express');
 const multer = require('multer');
-const path = require ('path');
+const path = require('path');
 const cors = require('cors');
 const app = express();
-const port = 6006;
+const port = process.env.PORT || 6006; 
 
-// In-memory database to store video information
 const videos = [];
 
-//multer for handling file uploads
+// Middleware for JSON parsing
+app.use(express.json());
+
+// Middleware for URL-encoded form data parsing
+app.use(express.urlencoded({ extended: true }));
+
 const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, 'uploads')
-    },
-    filename: (req, file, cb) => {
-      const uniqueID = Date.now() + '-' + Math.random().toString(36).substring(7);
-      const fileName = uniqueID + path.extname(file.originalname);
-      // Store the video information in the database
-      videos.push({ id: uniqueID, filename: fileName });
-      cb(null, fileName);
-    },
-  });
-    
-    const upload = multer({storage});
-   // Route for video upload
-app.post('/upload', upload.single('video'), (req, res) => {
-  const uploadedVideo = videos.find((video) => video.filename === req.file.filename);
-
-  if (!uploadedVideo) {
-    return res.status(500).json({ error: 'Error processing the uploaded video' });
-  }
-
-  res.json({ message: 'Video uploaded successfully', videoId: uploadedVideo.id });
+  destination: (req, file, cb) => {
+    cb(null, 'uploads');
+  },
+  filename: (req, file, cb) => {
+    const uniqueID = Date.now() + '-' + Math.random().toString(36).substring(7);
+    const fileName = uniqueID + path.extname(file.originalname);
+    videos.push({ id: uniqueID, filename: fileName });
+    cb(null, fileName);
+  },
 });
 
+const upload = multer({ storage });
 
-  // Route to get a video by its ID
+// Configure CORS to allow requests from a specific origin (e.g. frontend URL)
+const allowedOrigins = ['https://stage5-4qe0.onrender.com', 'http://localhost:6006']; 
+
+app.use(cors({ origin: '*' }));
+
+app.post('/upload', upload.single('video'), (req, res) => {
+  try {
+    const uploadedVideo = videos.find((video) => video.filename === req.file.filename);
+
+    if (!uploadedVideo) {
+      throw new Error('Error processing the uploaded video');
+    }
+
+    res.json({ message: 'Video uploaded successfully', videoId: uploadedVideo.id });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: error.message });
+  }
+});
+
 app.get('/video/:id', (req, res) => {
   const id = req.params.id;
   const videoInfo = videos.find((video) => video.id === id);
@@ -47,23 +58,13 @@ app.get('/video/:id', (req, res) => {
 
   const videoPath = path.join(__dirname, 'uploads', videoInfo.filename);
 
-  // Send the video file as a response
   res.sendFile(videoPath);
 });
-  // Route to get a list of all videos
+
 app.get('/videos', (req, res) => {
   const videoList = videos.map((video) => ({ id: video.id, filename: video.filename }));
   res.json({ videos: videoList });
 });
-app.use(cors());
-// app.use((req, res, next) => {
-//   res.setHeader('Access-Control-Allow-Origin', 'https://github.com');
-//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-//   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-//   res.setHeader('Access-Control-Allow-Credentials', true);
-//   next();
-// });
-
 
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
